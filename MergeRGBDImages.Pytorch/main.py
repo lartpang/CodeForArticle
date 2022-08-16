5f70ee5dfae7d06ec39421f5cafd304e09bf93a0
Pang <lartpang@163.com>
2022-08-16 20:49:25 +0800

    Update main.py

--- MergeRGBDImages.Pytorch/main.py
+++ MergeRGBDImages.Pytorch/main.py
@@ -4,38 +4,27 @@ import torch
 import torch.nn.functional as F
 
 
-def get_gaussian_kernel2d(kernel_size, sigma, normalize=True):
-    kernel_1d = cv2.getGaussianKernel(ksize=kernel_size, sigma=sigma, ktype=cv2.CV_32F)
-    kernel_2d = kernel_1d * kernel_1d.T
-    if normalize:
-        kernel_2d = kernel_2d / kernel_2d.sum()
-    return kernel_2d
+def get_log_kernel2d_pt(kernel_size, valid_ratio, normalize=False):
+    """为局部区域的加权求和生成2d的核权重。
 
+    Args:
+        kernel_size (int): 核的边长。
+        valid_ratio (torch.Tensor): 有效区间比例（基于边长的一半）。
+        normalize (bool, optional): 是否归一化输出。
 
-def get_customized_kernel2d(kernel_size, valid_ratio, normalize=False):
-    assert 0 <= valid_ratio <= 1
-    valid_ratio = -np.log(1 - valid_ratio)
-    valid_width = kernel_size // 2 * valid_ratio
-
-    kernel_1d = np.interp(
-        x=np.arange(-(kernel_size // 2), 1), xp=[-valid_width, 0], fp=[0, 1]
-    )
-    kernel_1d = np.concatenate([kernel_1d, kernel_1d[::-1][1:]], axis=-1)
-    kernel_1d = kernel_1d.reshape((1, -1))
-    kernel_2d = kernel_1d * kernel_1d.T
-
-    if normalize:
-        kernel_2d = kernel_2d / kernel_2d.sum()
-    return kernel_2d
-
-def get_customized_kernel2d_pt(kernel_size, valid_ratio, normalize=False):
+    Returns:
+        torch.Tensor: 生成的核 (kernel_size*kernel_size, H, W)
+    """
     assert valid_ratio.min() >= 0 and valid_ratio.max() <= 1
     ori_shape = valid_ratio.shape
     valid_ratio = -torch.log(1 - valid_ratio)
 
     valid_width = kernel_size // 2 * valid_ratio
-    coords = torch.arange(kernel_size).reshape(-1, 1, 1)
-    kernel_1d = F.relu(1 - torch.abs(coords / valid_width))
+    coords = torch.linspace(-(kernel_size//2), kernel_size//2, kernel_size).reshape(-1, 1, 1)
+
+    curr_position_mask = torch.zeros_like(coords)
+    curr_position_mask[kernel_size//2] = 1
+    kernel_1d = torch.where(valid_width == 0, curr_position_mask, F.relu(1 - torch.abs(coords / valid_width)))
     kernel_2d = torch.einsum("xhw,yhw->xyhw", kernel_1d, kernel_1d)
     kernel_2d = kernel_2d.reshape(-1, *ori_shape)
 
@@ -44,36 +33,31 @@ def get_customized_kernel2d_pt(kernel_size, valid_ratio, normalize=False):
     return kernel_2d
 
 
-image = "image.jpg"
-depth = "depth.png"
+image = r"E:\Coding\PythonTools\image_with_depth\rgbd\image_left1.jpg"
+# depth = r"E:\Coding\PythonTools\image_with_depth\rgbd\image_left1.png"
+depth = r"E:\Coding\PythonTools\image_with_depth\rgbd\mask.jpg"
 
 image = cv2.imread(image, cv2.IMREAD_COLOR)
 depth = cv2.imread(depth, cv2.IMREAD_GRAYSCALE)
-
-# ensure the same shape
 height, width, num_channels = image.shape
-depth = cv2.resize(depth, dsize=(width, height))
 
+depth = cv2.resize(depth, dsize=(width, height))
 image = torch.from_numpy(image).float()
 depth = torch.from_numpy(depth).float()
-
-# normalize the depth image
+depth = (depth < 128).float()
 depth = depth - depth.min()
 depth = depth / depth.max()
 
 kernel_size = 21
 tgt_image = torch.zeros_like(image, dtype=torch.float32)
 
-unfold_ops = torch.nn.Unfold(kernel_size=kernel_size)
-
 image = image.permute(2, 0, 1).unsqueeze(0)
 image = F.pad(image, pad=(kernel_size//2, kernel_size//2, kernel_size//2, kernel_size//2), mode='replicate')
-image = unfold_ops(image)
+image = F.unfold(image, kernel_size=kernel_size)
 image = image.reshape(num_channels, kernel_size*kernel_size, height, width)
-
-weight = get_customized_kernel2d_pt(kernel_size=kernel_size, valid_ratio=depth, normalize=True)
+weight = get_log_kernel2d_pt(kernel_size=kernel_size, valid_ratio=depth, normalize=True)
 
 tgt_image = (image * weight).sum(dim=1)
-
 tgt_image = tgt_image.permute(1, 2, 0).numpy()
-cv2.imwrite("merged_image.png", tgt_image.astype(np.uint8))
+
+cv2.imwrite("rgbd/merged_image.png", tgt_image.astype(np.uint8))
